5bd8c12ff8b9ced528733c4ab5608b868ddc9706
Ronll <lapushnerron@gmail.com>
2015-12-15 22:15:52 -0800

    Implemented client fetch

--- client/client/env/config.js
+++ client/client/env/config.js
@@ -7,7 +7,7 @@ if (!/(&|\?)username=/.test(window.location.search)) {
   if (newSearch !== '' & newSearch !== '?') {
     newSearch += '&';
   }
-  newSearch += 'username=anonymous');
+  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
   window.location.search = newSearch;
 }
 
